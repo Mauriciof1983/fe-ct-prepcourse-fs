f570ef9cc847cac159f2735fa66e7e80897a5ce1
MauricioF1983 <mauryf100@hotmail.com>
2024-01-26 11:09:38 -0300

    Ejercicios

--- M05 Fundamentos Javascript/ejercicios/20.js
+++ M05 Fundamentos Javascript/ejercicios/20.js
@@ -3,6 +3,18 @@ function esVocal(letra) {
   // Si el usuario ingresa un string de más de un caracter, debes retornar el mensaje: "Dato incorrecto".
   // Si no es vocal, tambien debe retornar "Dato incorrecto".
   // Tu código:
+  if (letra > 1){
+    console.log("Dato Incorrecto");
+    return("Dato incorrecto");
+}
+if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    console.log("Es vocal");
+    return ("Es vocal");
+}else {
+    console.log("Dato incorrecto");
+    return ("Dato incorrecto")
+}
+
 }
 
 module.exports = esVocal;
--- M05 Fundamentos Javascript/ejercicios/32.js
+++ M05 Fundamentos Javascript/ejercicios/32.js
@@ -2,6 +2,10 @@ function deEuroAdolar(euro) {
   // Debes calcular el valor recibido como argumento pasándolo a dólares.
   // Supongamos que 1 euro equivale a 1.20 dólares.
   // Tu código:
+  var conversion = 1.20;
+  dolar = euro * conversion;
+  return dolar;
+  
 }
 
 module.exports = deEuroAdolar;
